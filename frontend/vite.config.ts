61fea7f79469d47b3fe3ea14b8c934706f2770d9
leyban <leybanlazada@gmail.com>
2024-03-04 14:15:14 +0800

    new structure for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+      rollupOptions: {
+          input: {
+              home: resolve(__dirname, 'pages/index.html'),
+              main: resolve(__dirname, 'pages/main/index.html'),
+              test: resolve(__dirname, 'pages/test/index.html')
+          }
+      }
+  }
+
 })

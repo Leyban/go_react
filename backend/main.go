0373f750ed889f4425c3a26db3e7ae62bf70ff71
leyban <leybanlazada@gmail.com>
2024-03-04 14:14:04 +0800

    new structure for multipage app

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -9,6 +10,17 @@ func main() {
 	fs := http.FileServer(http.Dir("./../frontend/dist"))
 	http.Handle("/", fs)
 
+	http.HandleFunc("/api/data", dataHandler)
+
 	fmt.Println("Listening at port 8888")
 	http.ListenAndServe(":8888", nil)
+}
+
+type ApiResponse struct {
+	Message string `json:"message"`
+}
+
+func dataHandler(w http.ResponseWriter, r *http.Request) {
+	response := ApiResponse{Message: "Hello from the Golang API!"}
+	json.NewEncoder(w).Encode(response)
 }

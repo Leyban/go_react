a69121136ba2d666d3f58c10358245c4e60922e7
leyban <leybanlazada@gmail.com>
2024-03-04 12:06:24 +0800

    reset all

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,34 @@
-// import { BrowserRouter as Router, Route, } from 'react-router-dom'
-import Home from './pages/home/index'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 function App() {
-  const router = createBrowserRouter([
-  {
-    id: "root",
-    path: "/",
-    // loader() {
-      // Our root route always provides the user, if logged in
-      // return { user: fakeAuthProvider.username };
-    // },
-    Component: Home,
-    // children: [
-    //   {
-    //     index: true,
-    //     Component: PublicPage,
-    //   },
-    //   {
-    //     path: "login",
-    //     action: loginAction,
-    //     loader: loginLoader,
-    //     Component: LoginPage,
-    //   },
-    //   {
-    //     path: "protected",
-    //     loader: protectedLoader,
-    //     Component: ProtectedPage,
-    //   },
-    // ],
-  },
-  ])
+  const [count, setCount] = useState(0)
 
   return (
-      <RouterProvider router={router} />
-  // <Home />
-    // <Router>
-    //     <Route path="/" element={Home}/>
-    // </Router>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
